e803d7a99eeeb2d2a43be2078c3abbd54092aa0a
Mina Mekhael <mina.mekhael@ymail.com>
2019-04-04 19:22:55 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+
+  namespace :admin do
+    resources :stats, only: [:index]
+  end
+
   resources :authors, only: %i[show index] do
     resources :posts, only: %i[show index new edit]
   end
 
   resources :posts, only: %i[index show new create edit update]
 
-  get '/admin/stats', to: 'stats#index'
 
   root 'posts#index'
 end
